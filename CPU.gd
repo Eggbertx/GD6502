7a0e2688c06b3785aef352a12fc65cd9b3e5556b
Eggbertx <eggbertx@users.noreply.github.com>
2024-12-26 20:09:41 -0800

    Combine negative and zero flag update functions, as they are always called together

--- CPU.gd
+++ CPU.gd
@@ -227,10 +227,8 @@ func get_indirect_indexed_addr() -> int:
 	var zp := pop_byte()
 	return (get_word(zp) + Y) & 0xFFFF
 
-func _update_zero(register: int):
+func _update_zero_negative(register:int):
 	zero_flag = (register & 0xFF) == 0
-
-func _update_negative(register: int):
 	negative_flag = (register & 0x80) > 0
 
 func _update_carry_from_bit_0(val: int):
@@ -264,8 +262,7 @@ func _adc(val:int):
 		overflow_flag = ~(A ^ val) & (A ^ sum) & 0x80
 		carry_flag = sum & 0xFF00 > 0
 		A = sum & 0xFF
-	_update_negative(A)
-	_update_zero(A)
+	_update_zero_negative(A)
 
 func _sbc(val:int):
 	var diff := A - val - (1 - flags & flag_bit.CARRY)
@@ -283,8 +280,7 @@ func _sbc(val:int):
 	else:
 		A = diff & 0xFF
 	carry_flag = (diff & 0xFF00) == 0
-	_update_negative(A)
-	_update_zero(A)
+	_update_zero_negative(A)
 
 func _compare(val:int, reg:int):
 	carry_flag = reg >= val
@@ -295,23 +291,20 @@ func _rol(val:int) -> int:
 	var carry := flags & flag_bit.CARRY
 	flags = (flags & (~flag_bit.CARRY)) | ((val & 0x80) >> 7)
 	val = ((val << 1) & 0xFE) | carry
-	_update_negative(val)
-	_update_zero(val)
+	_update_zero_negative(val)
 	return val
 
 func _ror(val:int) -> int:
 	var carry := flags & flag_bit.CARRY
 	flags = (flags & (~flag_bit.CARRY)) | (val & 1)
 	val = (val >> 1) | (carry << 7)
-	_update_negative(val)
-	_update_zero(val)
+	_update_zero_negative(val)
 	return val
 
 func _lsr(val:int) -> int:
 	flags = (flags & (~flag_bit.CARRY)) | (val & 1)
 	val = val >> 1
-	_update_negative(val)
-	_update_zero(val)
+	_update_zero_negative(val)
 	return val
 
 ### This function can be overridden to handle opcodes differently than the standard implementation. Child classes
@@ -342,90 +335,76 @@ func execute(force = false, new_PC = -1):
 			set_status(status.STOPPED, true)
 		0x01: # ORA, indexed indirect
 			A |= get_byte(get_indexed_indirect_addr())
-			_update_negative(A)
-			_update_zero(A)
+			_update_zero_negative(A)
 		0x05: # ORA, zero page
 			A |= get_byte(pop_byte())
-			_update_negative(A)
-			_update_zero(A)
+			_update_zero_negative(A)
 		0x06: # ASL, zero page
 			var zp := pop_byte()
 			var num := get_byte(zp)
 			_update_carry_from_bit_0(num)
 			num = (num << 1) & 0xFF
 			set_byte(zp, num)
-			_update_negative(num)
-			_update_zero(num)
+			_update_zero_negative(num)
 		0x08: # PHP, implied
 			push_stack(flags)
 		0x09: # ORA, immediate
 			A |= pop_byte()
-			_update_negative(A)
-			_update_zero(A)
+			_update_zero_negative(A)
 		0x0A: # ASL, accumulator
 			_update_carry_from_bit_7(A)
 			A = (A << 1) & 0xFF
-			_update_negative(A)
-			_update_zero(A)
+			_update_zero_negative(A)
 		0x0D: # ORA, absolute
 			A |= get_byte(pop_word())
-			_update_negative(A)
-			_update_zero(A)
+			_update_zero_negative(A)
 		0x0E: # ASL, absolute
 			var addr := pop_word()
 			var num := get_byte(addr)
 			_update_carry_from_bit_0(num)
 			num = (num << 1) & 0xFF
 			set_byte(addr, num)
-			_update_negative(num)
-			_update_zero(num)
+			_update_zero_negative(num)
 		0x10: # BPL, relative
 			_branch(pop_byte(), !negative_flag)
 		0x11: # ORA, indirect indexed
 			A |= get_byte(get_indirect_indexed_addr())
-			_update_negative(A)
-			_update_zero(A)
+			_update_zero_negative(A)
 		0x15: # ORA, zero page, x
 			A |= get_byte(get_zpx_addr())
-			_update_negative(A)
-			_update_zero(A)
+			_update_zero_negative(A)
 		0x16: # ASL, zero page, x
 			var zp := get_zpx_addr()
 			var num := get_byte(zp)
 			_update_carry_from_bit_0(num)
 			num = (num << 1) & 0xFF
 			set_byte(zp, num)
-			_update_negative(num)
-			_update_zero(num)
+			_update_zero_negative(num)
 		0x18: # CLC, implied
 			carry_flag = false
 		0x19: # ORA, absolute, y
 			var addr := (pop_word() + Y) & 0xFFFF
 			var num := get_byte(addr)
 			A = (A | num) & 0xFF
-			_update_negative(A)
-			_update_zero(A)
+			_update_zero_negative(A)
 		0x1D: # ORA, absolute, x
 			var addr := (pop_word() + X) & 0xFFFF
 			var num := get_byte(addr)
 			A = (A | num) & 0xFF
-			_update_negative(A)
-			_update_zero(A)
+			_update_zero_negative(A)
 		0x1E: # ASL, absolute, x
 			var addr := (pop_word() + X) & 0xFFFF
 			var num := get_byte(addr)
 			_update_carry_from_bit_0(num)
 			num = (num << 1) & 0xFF
 			set_byte(addr, num)
-			_update_negative(num)
-			_update_zero(num)
+			_update_zero_negative(num)
 		0x20: # JSR, absolute
 			push_stack_addr(PC+1)
 			PC = pop_word()
 		0x21: # AND, indexed indirect
 			A &= get_byte(get_indexed_indirect_addr())
-			_update_negative(A)
-			_update_zero(A)
+			_update_zero_negative(A)
 		0x24: # BIT, zero page
 			var num := get_byte(pop_byte())
 			negative_flag = num & 0x80 == 0x80
@@ -434,8 +413,7 @@ func execute(force = false, new_PC = -1):
 		0x25: # AND, zero page
 			var num := get_byte(pop_byte())
 			A &= num
-			_update_negative(A)
-			_update_zero(A)
+			_update_zero_negative(A)
 		0x26: # ROL, zero page
 			var zp := pop_byte()
 			var val := get_byte(zp)
@@ -445,8 +423,7 @@ func execute(force = false, new_PC = -1):
 		0x29: # AND, immediate
 			var imm := pop_byte()
 			A = (A & imm) & 0xFF
-			_update_negative(A)
-			_update_zero(A)
+			_update_zero_negative(A)
 		0x2A: # ROL, accumulator
 			A = _rol(A)
 		0x2C: # BIT, absolute
@@ -457,8 +434,7 @@ func execute(force = false, new_PC = -1):
 		0x2D: # AND, absolute
 			var num := get_byte(pop_word())
 			A &= num
-			_update_negative(A)
-			_update_zero(A)
+			_update_zero_negative(A)
 		0x2E: # ROL, absolute
 			var addr := pop_word()
 			var val := get_byte(addr)
@@ -467,12 +443,10 @@ func execute(force = false, new_PC = -1):
 			_branch(pop_byte(), negative_flag)
 		0x31: # AND, indirect indexed
 			A &= get_byte(get_indirect_indexed_addr())
-			_update_negative(A)
-			_update_zero(A)
+			_update_zero_negative(A)
 		0x35: # AND, zero page, x
 			A &= get_byte(get_zpx_addr())
-			_update_negative(A)
-			_update_zero(A)
+			_update_zero_negative(A)
 		0x36: # ROL, zero page x
 			var zp := get_zpx_addr()
 			var val := get_byte(zp)
@@ -482,13 +456,11 @@ func execute(force = false, new_PC = -1):
 		0x39: # AND, absolute y
 			var addr := (pop_word() + Y) & 0xFFFF
 			A &= get_byte(addr)
-			_update_negative(A)
-			_update_zero(A)
+			_update_zero_negative(A)
 		0x3D: # AND, absolute x
 			var addr := (pop_word() + X) & 0xFFFF
 			A &= get_byte(addr)
-			_update_negative(A)
-			_update_zero(A)
+			_update_zero_negative(A)
 		0x3E: # ROL, absolute x
 			var addr := (pop_word() + X) & 0xFFFF
 			var val := get_byte(addr)
@@ -498,12 +470,10 @@ func execute(force = false, new_PC = -1):
 			PC = pop_stack_addr()
 		0x41: # EOR, indirect x
 			A ^= get_byte(get_indexed_indirect_addr())
-			_update_negative(A)
-			_update_zero(A)
+			_update_zero_negative(A)
 		0x45: # EOR, zero page
 			A ^= get_byte(pop_byte())
-			_update_negative(A)
-			_update_zero(A)
+			_update_zero_negative(A)
 		0x46: # LSR, zero page
 			var zp := pop_byte()
 			var num := get_byte(zp)
@@ -512,16 +482,14 @@ func execute(force = false, new_PC = -1):
 			push_stack(A)
 		0x49: # EOR, immediate
 			A ^= pop_byte()
-			_update_negative(A)
-			_update_zero(A)
+			_update_zero_negative(A)
 		0x4A: # LSR, accumulator
 			A = _lsr(A)
 		0x4C: # JMP, absolute
 			PC = pop_word()
 		0x4D: # EOR, absolute
 			A ^= get_byte(pop_word())
-			_update_negative(A)
-			_update_zero(A)
+			_update_zero_negative(A)
 		0x4E: # LSR, absolute
 			var addr := pop_word()
 			var num := get_byte(addr)
@@ -530,12 +498,10 @@ func execute(force = false, new_PC = -1):
 			_branch(pop_byte(), !overflow_flag)
 		0x51: # EOR, indirect y
 			A ^= get_byte(get_indirect_indexed_addr())
-			_update_negative(A)
-			_update_zero(A)
+			_update_zero_negative(A)
 		0x55: # EOR, zero page x
 			A ^= get_byte(get_zpx_addr())
-			_update_negative(A)
-			_update_zero(A)
+			_update_zero_negative(A)
 		0x56: # LSR, zero page x
 			var zp := get_zpx_addr()
 			var num := get_byte(zp)
@@ -544,12 +510,10 @@ func execute(force = false, new_PC = -1):
 			interrupt_flag = false
 		0x59: # EOR, absolute y
 			A ^= get_byte(pop_word() + Y)
-			_update_negative(A)
-			_update_zero(A)
+			_update_zero_negative(A)
 		0x5D: # EOR, absolute x
 			A ^= get_byte(pop_word() + X)
-			_update_negative(A)
-			_update_zero(A)
+			_update_zero_negative(A)
 		0x5E: # LSR, absolute x
 			var addr := pop_word() + X
 			var num := get_byte(addr)
@@ -618,12 +582,10 @@ func execute(force = false, new_PC = -1):
 			set_byte(pop_byte(), X)
 		0x88: # DEY, implied
 			Y = (Y - 1) & 0xFF
-			_update_negative(Y)
-			_update_zero(Y)
+			_update_zero_negative(Y)
 		0x8A: # TXA, implied
 			A = X
-			_update_negative(A)
-			_update_zero(A)
+			_update_zero_negative(A)
 		0x8C: # STY, absolute
 			set_byte(pop_word(), Y)
 		0x8D: # STA, absolute
@@ -642,8 +604,7 @@ func execute(force = false, new_PC = -1):
 			set_byte(get_zpy_addr(), X)
 		0x98: # TYA, implied
 			A = Y
-			_update_zero(A)
-			_update_negative(A)
+			_update_zero_negative(A)
 		0x99: # STA, absolute, y
 			set_byte(pop_word() + Y, A)
 		0x9A: # TXS, implied
@@ -652,90 +613,70 @@ func execute(force = false, new_PC = -1):
 			set_byte(pop_word() + X, A)
 		0xA0: # LDY, immediate
 			Y = pop_byte()
-			_update_zero(Y)
-			_update_negative(Y)
+			_update_zero_negative(Y)
 		0xA1: # LDA, indexed indirect
 			A = get_byte(get_indexed_indirect_addr())
-			_update_zero(A)
-			_update_negative(A)
+			_update_zero_negative(A)
 		0xA2: # LDX, immediate
 			X = pop_byte()
-			_update_zero(X)
-			_update_negative(X)
+			_update_zero_negative(X)
 		0xA4: # LDY, zero page
 			Y = get_byte(pop_byte())
-			_update_zero(Y)
-			_update_negative(Y)
+			_update_zero_negative(Y)
 		0xA5: # LDA, zero page
 			A = get_byte(pop_byte())
-			_update_zero(A)
-			_update_negative(A)
+			_update_zero_negative(A)
 		0xA6: # LDX, zero page
 			X = get_byte(pop_byte())
-			_update_zero(X)
-			_update_negative(X)
+			_update_zero_negative(X)
 		0xA8: # TAY, implied
 			Y = A
-			_update_zero(Y)
-			_update_negative(Y)
+			_update_zero_negative(Y)
 		0xA9: # LDA, immediate
 			A = pop_byte()
-			_update_zero(A)
-			_update_negative(A)
+			_update_zero_negative(A)
 		0xAA: # TAX, implied
 			X = A
-			_update_zero(X)
-			_update_negative(X)
+			_update_zero_negative(X)
 		0xAC: # LDY, absolute
 			Y = get_byte(pop_word())
-			_update_zero(Y)
-			_update_negative(Y)
+			_update_zero_negative(Y)
 		0xAD: # LDA, absolute
 			A = get_byte(pop_word())
-			_update_zero(A)
-			_update_negative(A)
+			_update_zero_negative(A)
 		0xAE: # LDX, absolute
 			X = get_byte(pop_word())
-			_update_zero(X)
-			_update_negative(X)
+			_update_zero_negative(X)
 		0xB0: # BCS, relative
 			_branch(pop_byte(), carry_flag)
 		0xB1: # LDA, indirect indexed
 			A = get_byte(get_indirect_indexed_addr())
-			_update_zero(A)
-			_update_negative(A)
+			_update_zero_negative(A)
 		0xB4: # LDY, zero page, x
 			Y = get_byte(get_zpx_addr())
-			_update_zero(Y)
-			_update_negative(Y)
+			_update_zero_negative(Y)
 		0xB5: # LDA, zero page, x
 			A = get_byte(get_zpx_addr())
-			_update_zero(A)
-			_update_negative(A)
+			_update_zero_negative(A)
 		0xB6: # LDX, zero page, y
 			X = get_byte(get_zpy_addr())
-			_update_zero(X)
-			_update_negative(X)
+			_update_zero_negative(X)
 		0xB8: # CLV, implied
 			overflow_flag = false
 		0xB9: # LDA, absolute, y
 			A = get_byte(pop_word() + Y)
-			_update_zero(A)
-			_update_negative(A)
+			_update_zero_negative(A)
 		0xBA: # TSX, implied
 			X = SP
 		0xBC: # LDY, absolute, x
 			Y = get_byte(pop_word() + X)
-			_update_zero(Y)
-			_update_negative(Y)
+			_update_zero_negative(Y)
 		0xBD: # LDA, absolute, x
 			A = get_byte(pop_word() + X)
-			_update_zero(A)
-			_update_negative(A)
+			_update_zero_negative(A)
 		0xBE: # LDX, absolute, y
 			X = get_byte(pop_word())
-			_update_zero(Y)
-			_update_negative(Y)
+			_update_zero_negative(Y)
 		0xC0: # CPY, immediate
 			_compare(pop_byte(), Y)
 		0xC1: # CMP, indirect x
@@ -751,18 +692,15 @@ func execute(force = false, new_PC = -1):
 			var zp := pop_byte()
 			var new_val := (get_byte(zp) - 1) & 0xFF
 			set_byte(zp, new_val)
-			_update_zero(new_val)
-			_update_negative(new_val)
+			_update_zero_negative(new_val)
 		0xC8: # INY, implied
 			Y = (Y + 1) & 0xFF
-			_update_zero(Y)
-			_update_negative(Y)
+			_update_zero_negative(Y)
 		0xC9: # CMP, immediate
 			_compare(pop_byte(), A)
 		0xCA: # DEX, implied
 			X = (X - 1) & 0xFF
-			_update_zero(X)
-			_update_negative(X)
+			_update_zero_negative(X)
 		0xCC: # CPY, absolute
 			var addr := pop_word()
 			_compare(get_byte(addr), Y)
@@ -773,8 +711,7 @@ func execute(force = false, new_PC = -1):
 			var addr := pop_word()
 			var new_val := (get_byte(addr) - 1) & 0xFF
 			set_byte(addr, new_val)
-			_update_zero(new_val)
-			_update_negative(new_val)
+			_update_zero_negative(new_val)
 		0xD0: # BNE, relative
 			_branch(pop_byte(), !zero_flag)
 		0xD1: # CMP, indirect y
@@ -788,8 +725,7 @@ func execute(force = false, new_PC = -1):
 			var zp := get_zpx_addr()
 			var new_val := (get_byte(zp) - 1) & 0xFF
 			set_byte(zp, new_val)
-			_update_zero(new_val)
-			_update_negative(new_val)
+			_update_zero_negative(new_val)
 		0xD8: # CLD, implied
 			decimal_flag = false
 		0xD9: # CMP, absolute y
@@ -802,8 +738,7 @@ func execute(force = false, new_PC = -1):
 			var addr := (pop_word() + X) & 0xFFFF
 			var new_val := (get_byte(addr) - 1) & 0xFF
 			set_byte(addr, new_val)
-			_update_zero(new_val)
-			_update_negative(new_val)
+			_update_zero_negative(new_val)
 		0xE0: # CPX, immediate
 			_compare(pop_byte(), X)
 		0xE1: # SBC, indirect x
@@ -819,12 +754,10 @@ func execute(force = false, new_PC = -1):
 			var zp := pop_byte()
 			var new_val := (get_byte(zp) + 1) & 0xFF
 			set_byte(zp, new_val)
-			_update_negative(new_val)
-			_update_zero(new_val)
+			_update_zero_negative(new_val)
 		0xE8: # INX, implied
 			X = (X + 1) & 0xFF
-			_update_zero(X)
-			_update_negative(X)
+			_update_zero_negative(X)
 		0xE9: # SBC, immediate
 			_sbc(pop_byte())
 		0xEA: # NOP, implied
@@ -839,8 +772,7 @@ func execute(force = false, new_PC = -1):
 			var addr := pop_word()
 			var new_val := (get_byte(addr) + 1) & 0xFF
 			set_byte(addr, new_val)
-			_update_negative(new_val)
-			_update_zero(new_val)
+			_update_zero_negative(new_val)
 		0xF0: # BEQ, relative
 			_branch(pop_byte(), zero_flag)
 		0xF1: # SBC, indirect y
@@ -853,8 +785,7 @@ func execute(force = false, new_PC = -1):
 			var zp := get_zpx_addr()
 			var new_val := (get_byte(zp) + 1) & 0xFF
 			set_byte(zp, new_val)
-			_update_negative(new_val)
-			_update_zero(new_val)
+			_update_zero_negative(new_val)
 		0xF8: # SED, implied
 			decimal_flag = true
 		0xF9: # SBC, absolute y
@@ -867,8 +798,6 @@ func execute(force = false, new_PC = -1):
 			var addr := pop_word() + X
 			var new_val := (get_byte(addr) + 1) & 0xFF
 			set_byte(addr, new_val)
-			_update_negative(new_val)
-			_update_zero(new_val)
+			_update_zero_negative(new_val)
 		_:
 			illegal_opcode.emit(current_opcode)
-
